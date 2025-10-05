4e82910189cb3122ea8dff293c4e2f669215d95b
Yash Raj <the.yashh@icloud.com>
2025-10-05 19:37:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	eslint: {
+		// Skip ESLint during production builds (vercel and local `next build`)
+		ignoreDuringBuilds: true,
+	},
+};
 
 export default nextConfig;
